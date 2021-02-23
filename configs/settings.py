4d968921c385f72e6c10374f6f46d034e8dc0ee5
ranthy <realthingshakes25@gmail.com>
2021-02-23 17:51:10 +0900

    Modified the flag for DEBUG to LOCAL_FLG in settings.py

--- configs/settings.py
+++ configs/settings.py
@@ -20,6 +20,7 @@ except ImportError:
     pass
 
 DEBUG = True
+LOCAL_FLG = False
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -116,7 +117,7 @@ STATIC_URL = '/static/'
 
 
 
-if not DEBUG:
+if not LOCAL_FLG:
     SECRET_KEY = os.environ['SECRET_KEY']
     import django_heroku
     django_heroku.settings(locals())
